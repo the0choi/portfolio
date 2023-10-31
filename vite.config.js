dec17b88c1d8156f78fa9f81cfd90286f6ab3b48
the0choi <theodore.choi07@gmail.com>
2023-10-31 19:20:33 +1100

    merge minor changes

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',
   plugins: [react()],
-  base: '/portfolio/',
 })
