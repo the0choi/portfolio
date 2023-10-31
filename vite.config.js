f83d4a730840a9b61bd0287591189fb3ab2459c0
the0choi <theodore.choi07@gmail.com>
2023-10-31 20:09:16 +1100

    fix vite base

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import process from 'process'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',
+  base: '/portfolio/',
   plugins: [react()],
 })
